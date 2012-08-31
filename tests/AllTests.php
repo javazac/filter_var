7117865f5cf74798378e746c2b7f58ff5c639a17
Zac Konopa <zac@cleverconcepts.net>
2012-08-31 13:15:38 -0700

    Add all tests to the AllTests test suite.

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -1,10 +1,26 @@
 <?php
 require_once 'simpletest/autorun.php';
+require_once 'TestBooleanFilterVar.php';
+require_once 'TestConstants.php';
+require_once 'TestFilterHasVar.php';
+require_once 'TestFilterId.php';
+require_once 'TestFilterVar.php';
+require_once 'TestIPFilterVar.php';
+require_once 'TestNumericFilterVar.php';
+require_once 'TestRegexpFilterVar.php';
 
 class AllTests extends TestSuite {
 
 	function __construct() {
 
 		parent::__construct();
+
+		$this->add(new TestBooleanFilterVar());
+		$this->add(new TestFilterHasVar());
+		$this->add(new TestFilterId());
+		$this->add(new TestFilterVar());
+		$this->add(new TestIPFilterVar());
+		$this->add(new TestNumericFilterVar());
+		$this->add(new TestRegexpFilterVar());
 	}
 }
