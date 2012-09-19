b4a6cbf33a93c2ccf084cbb667d3d4bb98a21db2
Zac Konopa <zac@cleverconcepts.net>
2012-09-19 13:02:26 -0700

    Added first test to FILTER_VALIDATE_URL

--- tests/TestValidateUrlFilterVar.php
+++ tests/TestValidateUrlFilterVar.php
@@ -3,5 +3,9 @@ require_once 'AbstractFilterVarTest.php';
 
 class TestValidateUrlFilterVar extends AbstractFilterVarTest
 {
-
+	public function testValidateUrlNoOpts()
+	{
+		$url = 'http://www.google.com';
+		$this->assertTrue(filter_var($url, FILTER_VALIDATE_URL) === $url, 'FILTER_VALIDATE_URL failed');
+	}
 }//end class TestValidateUrlFilterVar
